5d3a614dfaa910b84c4f8a0141d51b89f2cdc131
Dave Ernsting <malfernion@gmail.com>
2017-11-19 20:49:52 +0000

    now using paper ripple to give better feedback when pressing the button

--- piServer.js
+++ piServer.js
@@ -1,8 +1,12 @@
-var express = require("express");
+var express = require('express');
+var path = require('path');
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use('/paper-ripple', express.static(path.join(__dirname + '/node_modules/paper-ripple/dist')));
+
 app.get("/", function(req, res) {
-   res.sendfile('piBooth.html');
+   res.sendFile('piBooth.html', {root : __dirname});
 });
 
 app.post("/capture", function(req, res) {
